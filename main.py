f9a45a9afcb1e51c96fc7cf5d9026dc04361384c
Anandu07 <anandukb0707@gmail.com>
2022-09-13 14:16:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 fakedb = []
 
 class Course(BaseModel):
